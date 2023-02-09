85fa6d80d51330ce0b39d463dd199fd873268276
cathautzen <cathautzen@gmail.com>
2023-02-09 13:53:11 +0100

    git init

---
+++ includes/footer.php
@@ -0,0 +1,19 @@
+<div class="footer">
+    <div>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+    </div>
+
+    <div>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+    </div>
+
+    <div>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+    </div>
+</div>
